b18d2cbd7d95eefea66953189468971c6cabe58a
hrJin9 <bushyerin97@gmail.com>
2023-12-07 16:32:54 +0900

    feat: redis repository 구현

--- src/main/java/com/todos/mmd/global/config/RedisConfig.java
+++ src/main/java/com/todos/mmd/global/config/RedisConfig.java
@@ -25,11 +25,10 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+    public RedisTemplate<byte[], byte[]> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
+
 }
