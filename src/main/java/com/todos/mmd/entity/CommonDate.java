f01ea558f7bc300ca9faedeba465da650357c449
hrJin9 <bushyerin97@gmail.com>
2023-12-11 17:10:35 +0900

    fix: Jpa Auditing 설정

--- src/main/java/com/todos/mmd/entity/CommonDate.java
+++ src/main/java/com/todos/mmd/entity/CommonDate.java
@@ -6,34 +6,18 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class Common {
+public class CommonDate {
 
     @CreatedDate
     private LocalDateTime createdDate;
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
-
-    @Enumerated(EnumType.STRING)
-    private UseStatus useStatus;
-
-    public enum UseStatus {
-        Y, N
-    }
-
-
-    public Common() {
-        this.createdDate = LocalDateTime.now();
-        this.lastModifiedDate = LocalDateTime.now();
-        this.useStatus = UseStatus.Y;
-    }
 
 }
