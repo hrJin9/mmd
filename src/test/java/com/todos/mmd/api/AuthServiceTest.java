0be61d185c698ca604199123109d31ba39e2d4cd
hrJin9 <bushyerin97@gmail.com>
2024-01-02 19:47:03 +0900

    test: 로그인/회원가입 테스트코드 작성

--- src/test/java/com/todos/mmd/api/AuthServiceTest.java
+++ src/test/java/com/todos/mmd/api/AuthServiceTest.java
@@ -1,92 +1,221 @@
 package com.todos.mmd.api;
 
-import com.todos.mmd.auth.api.request.AuthRequest;
+import com.todos.mmd.auth.api.response.TokenResponse;
 import com.todos.mmd.auth.application.AuthService;
+import com.todos.mmd.auth.application.MemberDetails;
+import com.todos.mmd.auth.application.dto.LoginDto;
 import com.todos.mmd.auth.application.dto.MemberCreateDto;
 import com.todos.mmd.auth.application.util.JwtTokenProvider;
+import com.todos.mmd.auth.application.util.PasswordEncryptor;
 import com.todos.mmd.auth.domain.Member;
+import com.todos.mmd.auth.domain.MemberRole;
+import com.todos.mmd.auth.domain.RefreshToken;
+import com.todos.mmd.auth.domain.UseStatus;
+import com.todos.mmd.auth.exception.AuthException;
+import com.todos.mmd.auth.exception.JwtException;
 import com.todos.mmd.global.exception.EmailException;
 import com.todos.mmd.repository.member.MemberRepository;
 import com.todos.mmd.repository.redis.RefreshTokenRepository;
 import com.todos.mmd.support.ServiceTest;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @DisplayName("회원가입/로그인 서비스 테스트")
 @ServiceTest
 class AuthServiceTest {
-    @MockBean
+
+    @InjectMocks
     private AuthService authService;
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
-    @Autowired
+    @Mock
     private RefreshTokenRepository refreshTokenRepository;
 
-    @Autowired
+    @Mock
     private JwtTokenProvider jwtTokenProvider;
 
-    @AfterEach
-    public void cleanUp() {memberRepository.deleteAllInBatch();}
+    private static Member existMember;
+
+    @BeforeAll
+    static void setUp() {
+        existMember = new Member("tester@gmail.com", PasswordEncryptor.encrypt("tester123!@#"), "테스터", "01011112222", "테스터주소", MemberRole.USER, UseStatus.Y);
+    }
+
+    @Test
+    public void 이미_존재하는_이메일이면_예외를_던진다() {
+        // given
+        MemberCreateDto 새로운_회원 = new MemberCreateDto(
+                "tester@gmail.com",
+                "test123123#",
+                "테스터",
+                "01011112222",
+                "테스터 주소"
+        );
+
+        given(memberRepository.findByEmail(existMember.getEmail()))
+                .willReturn(Optional.of(existMember));
+
+        // when, then
+        assertThatThrownBy(() -> authService.register(새로운_회원))
+                .isInstanceOf(EmailException.class)
+                .hasMessage("중복된 이메일입니다.");
+    }
 
     @Test
     public void 정상적인_회원을_저장한다() {
         // given
-        final AuthRequest.MemberCreateRequest 새로운_회원 = new AuthRequest.MemberCreateRequest(
-                "tester@google.com",
-                "tester123!@#",
+        MemberCreateDto 새로운_회원 = new MemberCreateDto(
+                "newtester@gmail.com",
+                "test123123#",
                 "테스터",
                 "01011112222",
                 "테스터 주소"
         );
 
         // when
-        final Member 저장된_회원 = authService.register(MemberCreateDto.from(새로운_회원));
+        when(memberRepository.save(any())).thenReturn(Member.from(새로운_회원));
+        Member savedMember = authService.register(새로운_회원);
 
         // then
-        assertThat(새로운_회원.getEmail()).isEqualTo(저장된_회원.getEmail());
-        assertThat(새로운_회원.getName()).isEqualTo(저장된_회원.getName());
+        then(memberRepository).should(times(1)).save(any());
+        assertThat(savedMember.getEmail()).isEqualTo(새로운_회원.getEmail());
     }
 
-    @Test
-    public void 관리자_가입_요청_성공() {
+   @Test
+   public void 이메일이_존재하지_않으면_예외를_던진다() {
+       // given
+       LoginDto 로그인_회원 = new LoginDto("notester@gmail.com", "tester123!@#");
 
-    }
+       given(memberRepository.findByEmail(로그인_회원.getEmail()))
+               .willReturn(Optional.empty());
+
+       // when, then
+       assertThatThrownBy(() -> authService.login(로그인_회원))
+               .isInstanceOf(EmailException.class)
+               .hasMessage("존재하지 않는 이메일입니다.");
+   }
 
     @Test
-    public void 주소를_제외한_모든값들은_포함되어야_한다() {
+    public void 비밀번호가_일치하지_않으면_예외를_던진다() {
         // given
-//        authService.register(MemberCreateDto.from(일반회원_가입요청));
+        LoginDto 로그인_회원 = new LoginDto("tester@gmail.com", "wrongPassword");
 
+        given(memberRepository.findByEmail(existMember.getEmail()))
+            .willReturn(Optional.of(existMember));
+
+        // when, then
+        assertThatThrownBy(() -> authService.login(로그인_회원))
+                .isInstanceOf(AuthException.class)
+                .hasMessage("이메일 또는 비밀번호가 일치하지 않습니다.");
     }
 
     @Test
-    public void 비밀번호는_8자이상_15자이하로_영어숫자특수문자를_포함해야한다() {
-
-    }
-
-
-    @Test
-    public void 중복_이메일은_등록할_수_없다() {
+    public void 비밀번호가_일치하면_토큰을_반환한다() {
         // given
-        final AuthRequest.MemberCreateRequest 중복된_새로운_회원 = new AuthRequest.MemberCreateRequest(
-                "tester@google.com",
-                "tester123!@#",
-                "테스터2",
-                "01011112222",
-                "테스터 주소"
-        );
+        LoginDto 로그인_회원 = new LoginDto("tester@gmail.com", "tester123!@#");
+
+        given(memberRepository.findByEmail(existMember.getEmail()))
+                .willReturn(Optional.of(existMember));
+        given(jwtTokenProvider.generate(existMember.getEmail(), existMember.getRole().toString()))
+                .willReturn(new TokenResponse("atoken", "rtoken", "b", 1L));
 
         // when
-        assertThatThrownBy(() -> authService.register(MemberCreateDto.from(중복된_새로운_회원)))
-                .isInstanceOf(EmailException.class)
-                .hasMessage("중복된 이메일입니다.");
+        TokenResponse response = authService.login(로그인_회원);
+
+        // then
+        then(refreshTokenRepository).should(times(1)).save(any());
+        assertThat(response.getAccessToken()).isEqualTo("atoken");
     }
+
+    @Test
+    public void 억세스토큰_재발급시_해당_이메일의_리프레시_토큰이_없다면_예외를_던진다() {
+        // given
+        MemberDetails memberDetails = new MemberDetails(existMember);
+
+        given(refreshTokenRepository.findById(memberDetails.getUsername()))
+                .willReturn(Optional.empty());
+
+        // TODO : 왜 에러가 안던져지는지 모르겠음..
+        // when, then
+        assertThatThrownBy(() -> authService.reissueAccessToken(memberDetails))
+                .isInstanceOf(JwtException.class)
+                .hasMessage("만료된 토큰입니다.");
+    }
+
+    @Test
+    public void 해당_이메일의_리프레시토큰이_있다면_재발급한_억세스토큰을_반환한다() {
+        // given
+        MemberDetails memberDetails = new MemberDetails(existMember);
+
+        given(jwtTokenProvider.reissueAccessToken(memberDetails.getUsername(), memberDetails.getAuthorities().toString()))
+                .willReturn(new TokenResponse("atoken", "rtoken", "b", 1L));
+
+        // when
+        TokenResponse tokenResponse = authService.reissueAccessToken(memberDetails);
+
+        // then
+        assertThat(tokenResponse.getAccessToken()).isEqualTo("atoken");
+        assertThat(tokenResponse.getRefreshToken()).isEqualTo("rtoken");
+    }
+
+    @Test
+    public void 로그아웃시_해당_이메일의_리프레시토큰이_없다면_예외를_던진다() {
+        // given
+        String email = existMember.getEmail();
+        String refreshToken = "rtoken";
+
+        given(refreshTokenRepository.findById(email))
+                .willReturn(Optional.empty());
+
+        // when, then
+        assertThatThrownBy(() -> authService.logout(email, refreshToken))
+                .isInstanceOf(JwtException.class)
+                .hasMessage("이미 로그아웃된 사용자입니다.");
+    }
+
+    @Test
+    public void 로그아웃시_해당_이메일의_리프레시토큰이_아니라면_예외를_던진다() {
+        // given
+        String email = existMember.getEmail();
+        String refreshToken = "wrongToken";
+
+        given(refreshTokenRepository.findById(email))
+                .willReturn(Optional.of(new RefreshToken("wrong@gmail.com", "rtoken", 1L)));
+
+        // when, then
+        assertThatThrownBy(() -> authService.logout(email, refreshToken))
+                .isInstanceOf(JwtException.class)
+                .hasMessage("로그인된 사용자의 Refresh 토큰이 아닙니다.");
+    }
+
+    @Test
+    public void 로그아웃시_해당_이메일의_리프레시토큰이라면_삭제한다() {
+        // given
+        String email = existMember.getEmail();
+        String refreshToken = "rtoken";
+
+        given(refreshTokenRepository.findById(email))
+                .willReturn(Optional.of(new RefreshToken(existMember.getEmail(), refreshToken, 1L)));
+
+        // when
+        authService.logout(email, refreshToken);
+
+        // then
+        then(refreshTokenRepository).should(times(1)).delete(any());
+    }
+
 }
--- src/test/java/com/todos/mmd/support/ServiceTest.java
+++ src/test/java/com/todos/mmd/support/ServiceTest.java
@@ -1,17 +1,17 @@
 package com.todos.mmd.support;
 
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-//@Target(ElementType.TYPE)
-//@Retention(RetentionPolicy.RUNTIME)
-//@Documented
-//@Inherited
-//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@SpringBootTest
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(MockitoExtension.class)
 public @interface ServiceTest {
 }
