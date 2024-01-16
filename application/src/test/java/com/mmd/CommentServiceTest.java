d3028c3874953c987a83c33a8bf15922bf17eb38
hrJin9 <bushyerin97@gmail.com>
2024-01-16 17:09:40 +0900

    test: 조회 api 테스트

---
+++ application/src/test/java/com/mmd/CommentServiceTest.java
@@ -0,0 +1,54 @@
+package com.mmd;
+
+import com.mmd.comment.CommentService;
+import com.mmd.domain.MemberRole;
+import com.mmd.domain.UseStatus;
+import com.mmd.entity.Member;
+import com.mmd.repository.CommentRepository;
+import com.mmd.support.ServiceTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+@DisplayName("코멘트 서비스 테스트")
+@ServiceTest
+class CommentServiceTest {
+    private static Member member;
+
+    @InjectMocks
+    private CommentService commentService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @BeforeAll
+    static void setUp() {
+        member = new Member(1L, "tester@gmail.com", "password123!@#", "테스터", "테스터닉네임", "01011112222", "테스터 주소", MemberRole.USER, UseStatus.IN_USE);
+
+    }
+
+
+    @Test
+    public void 해당_다이어리의_코멘트를_가져온다() {
+        // given
+//        given(commentRepository.findAllComment(any()))
+//                .willReturn();
+
+        // when, then
+
+
+    }
+
+}
