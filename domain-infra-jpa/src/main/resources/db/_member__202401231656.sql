f59fdfa971ecd934346c46903dde8742a7b05891
hrJin9 <bushyerin97@gmail.com>
2024-01-23 16:58:36 +0900

    docs: friend db sql

---
+++ domain-infra-jpa/src/main/resources/db/_member__202401231656.sql
@@ -0,0 +1,11 @@
+INSERT INTO mmd.`member` (created_date,last_modified_date,address,email,name,nick_name,password,phone,`role`,use_status) VALUES
+	 ('2024-01-22 15:01:32.552605','2024-01-22 15:01:32.552605',NULL,'dallae@gmail.com','진혜린','달래','KEN0xVeDYF5xEKUaKVMSP7ftEYzDWrkA+6S2dAQ4kaE=','010-1234-1234','USER','IN_USE'),
+	 ('2024-01-22 14:38:25.048878','2024-01-22 14:38:25.048878',NULL,'mmd_member@naver.com','테멤','테스트멤버','5y9ZzIBwEil6H7Wbxx/34zooZVS/TcN+2nFALCaH7ho=','010-1234-1234','USER','IN_USE'),
+	 ('2024-01-22 15:01:32.552605','2024-01-22 15:01:32.552605',NULL,'mmd_member2@naver.com','테멤2','테스트멤버2','LL+X/4s9V9VbEPQn4NgZnxDQ0tEHKLGbu253EjslmXs=','010-1234-1235','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member3@naver.com','테멤3','테스트멤버3','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member4@gmail.com','조희진','픠진이','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member5@gmail.com','구민영','핌영이','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member6@gmail.com','홍수진','푸진이','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member7@gmail.com','원희진','늬진이','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member8@gmail.com','테멤8','테스트8','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE'),
+	 ('2024-01-22 15:01:42.312960','2024-01-22 15:01:42.312960',NULL,'mmd_member9@gmail.com','테멤9','테스트9','3xfFj1plpmqKKT4oiDxqAARJVU/3/MxVRu9GfDOiitI=','010-1234-1236','USER','IN_USE');
