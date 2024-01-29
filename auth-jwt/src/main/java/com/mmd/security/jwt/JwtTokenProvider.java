ee86db3b8da090bf4d0567ed3c1bf14c13bd5c1d
hrJin9 <bushyerin97@gmail.com>
2024-01-29 16:53:21 +0900

    fix: 토큰 유효시간 편의성을 위해 늘림

--- auth-jwt/src/main/java/com/mmd/security/jwt/JwtTokenProvider.java
+++ auth-jwt/src/main/java/com/mmd/security/jwt/JwtTokenProvider.java
@@ -23,8 +23,8 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 1;        // 1시간
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 30;        // 30일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 30;  // 30일
     private static final String AUTHORITIES_KEY = "auth";
     private final Key key;
     private final MemberDetailsService memberDetailsService;
