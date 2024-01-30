031194a13f3adf34aa040b919274ac30bfbccb38
hrJin9 <bushyerin97@gmail.com>
2024-01-30 15:01:31 +0900

    feat: 400 에러 exception handling

--- api-diary/src/main/java/com/mmd/exception/ApiExceptionAdvice.java
+++ api-diary/src/main/java/com/mmd/exception/ApiExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.mmd.exception;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,12 @@ public class ApiExceptionAdvice {
     @ExceptionHandler(MmdApiException.class)
     public ResponseEntity<ExceptionResponse> handleMmdApiException(MmdApiException e) {
         return ResponseEntity.status(e.getStatus())
+                .body(new ExceptionResponse(e.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ResponseEntity<ExceptionResponse> handlerBadRequestExcception(HttpClientErrorException.BadRequest e) {
+        return ResponseEntity.status(e.getStatusCode())
                 .body(new ExceptionResponse(e.getMessage(), LocalDateTime.now()));
     }
 }
