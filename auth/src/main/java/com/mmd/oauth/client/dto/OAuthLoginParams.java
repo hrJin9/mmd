a8ce63b5f67cfd225d1cf9e5ec680490e3bdf842
hrJin9 <bushyerin97@gmail.com>
2024-03-06 17:12:03 +0900

    feat: oauth client 로그인 param dto 정의

---
+++ auth/src/main/java/com/mmd/oauth/client/dto/OAuthLoginParams.java
@@ -0,0 +1,9 @@
+package com.mmd.oauth.client.dto;
+
+import com.mmd.domain.OAuthProvider;
+import org.springframework.util.MultiValueMap;
+
+public interface OAuthLoginParams {
+    OAuthProvider oAuthProvider();
+    MultiValueMap<String, String> makeBody();
+}
