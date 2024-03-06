551091cd5572479035dd0976dd0c0d30056d8543
hrJin9 <bushyerin97@gmail.com>
2024-03-06 17:12:37 +0900

    refactor: oauth 로그인 로직 수정

--- api-diary/src/main/java/com/mmd/auth/OAuthController.java
+++ api-diary/src/main/java/com/mmd/auth/OAuthController.java
@@ -1,10 +1,10 @@
 package com.mmd.auth;
 
+import com.mmd.domain.OAuthProvider;
 import com.mmd.oauth.application.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -16,7 +16,7 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/login/{oAuthProvider}")
-    public ResponseEntity<Void> oauthLogin(@PathVariable String oAuthProvider) {
+    public ResponseEntity<Void> oauthLogin(@PathVariable OAuthProvider oAuthProvider) {
         String redirectUri = oAuthService.findLoginRedirectUri(oAuthProvider);
 
         return ResponseEntity.status(HttpStatus.FOUND)
@@ -25,8 +25,9 @@ public class OAuthController {
     }
 
     @GetMapping("/{oauthProvider}")
-    public ResponseEntity<Void> OAuth2Register(@RequestParam String authorizationCode) {
+    public ResponseEntity<String> OAuth2Register(@PathVariable OAuthProvider oauthProvider,
+                                                 @RequestParam String authorizationCode) {
 //        authService.updateOAuth2MemberRole(email);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(oAuthService.login(oauthProvider, authorizationCode));
     }
 }
--- auth/src/main/java/com/mmd/oauth/application/OAuthService.java
+++ auth/src/main/java/com/mmd/oauth/application/OAuthService.java
@@ -1,32 +1,48 @@
 package com.mmd.oauth.application;
 
 import com.mmd.domain.OAuthProvider;
-import com.mmd.oauth.client.dto.GoogleProviderInfo;
-import com.mmd.oauth.client.dto.KakakoProviderInfo;
-import com.mmd.oauth.client.dto.NaverProviderInfo;
-import com.mmd.oauth.client.dto.OAuthProviderInfo;
+import com.mmd.oauth.client.dto.*;
+import com.mmd.oauth.client.dto.google.GoogleLoginParams;
+import com.mmd.oauth.client.dto.google.GoogleProviderInfo;
+import com.mmd.oauth.client.dto.kakao.KakaoProviderInfo;
+import com.mmd.oauth.client.dto.naver.NaverLoginParams;
+import com.mmd.oauth.client.dto.naver.NaverProviderInfo;
+import com.mmd.oauth.client.request.OAuthApiClient;
+import com.mmd.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Slf4j
-@RequiredArgsConstructor
 @Service
 public class OAuthService {
+    private final MemberRepository memberRepository;
+    private final Map<OAuthProvider, OAuthProviderInfo> providers;
+    private final Map<OAuthProvider, OAuthLoginParams> params;
+
+    public OAuthService(MemberRepository memberRepository,
+                        List<OAuthProviderInfo> providers,
+                        List<OAuthLoginParams> params) {
+        this.memberRepository = memberRepository;
+        this.providers = providers.stream().collect(Collectors.toUnmodifiableMap(OAuthProviderInfo::oAuthProvider, Function.identity()));
+        this.params = params.stream().collect(Collectors.toUnmodifiableMap(OAuthLoginParams::oAuthProvider, Function.identity()));
+    }
 
     /* oauth 서버에 따른 redirect Uri를 알아온다. */
-    public String findLoginRedirectUri(String oAuthProvider) {
-        OAuthProviderInfo oAuthProviderInfo = getOAuthProviderInfo(oAuthProvider);
+    public String findLoginRedirectUri(OAuthProvider oAuthProvider) {
+        OAuthProviderInfo oAuthProviderInfo = providers.get(oAuthProvider);
         return oAuthProviderInfo.getRedirectUri();
     }
 
-    private OAuthProviderInfo getOAuthProviderInfo(String oAuthProvider) {
-        if(oAuthProvider.equalsIgnoreCase(OAuthProvider.GOOGLE.toString())) {
-            return new GoogleProviderInfo();
-        }
-        if (oAuthProvider.equalsIgnoreCase(OAuthProvider.KAKAO.toString())) {
-            return new KakakoProviderInfo();
-        }
-        return new NaverProviderInfo();
+    /* OAuth 로그인한다. */
+    public String login(OAuthProvider oAuthProvider, String authorizationCode) {
+        OAuthLoginParams oAuthLoginParams = params.get(oAuthProvider);
+        return null;
     }
+
 }
